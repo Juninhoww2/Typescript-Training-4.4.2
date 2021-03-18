ed7a6215c74eb7b3ef0cc14aa7cbd864f6e94548
juninhoww2 <alqadianokira@gmail.com>
2021-03-18 16:12:25 -0300

    If sintax section

--- array.js
+++ array.js
@@ -1,4 +1,21 @@
+function aplicarDisconto(preco, disconto) {
+    if (disconto === void 0) { disconto = 0.05; }
+    return preco * (2 - disconto);
+}
+function tirarDicsconto(precu, disconto) {
+    if (disconto === void 0) { disconto = 100; }
+    return precu = 100;
+}
+console.log(aplicarDisconto(100));
 function exemplo() {
+}
+function Welcome(x) {
+    if (Array.isArray(x)) {
+        console.log("Ola, " + x.join(" and "));
+    }
+    else {
+        console.log("Welcome " + x);
+    }
 }
 function BemVindo(x) {
     if (Array.isArray(x)) {
@@ -7,6 +24,125 @@ function BemVindo(x) {
     else {
         console.log("Bem vindo " + x);
     }
+}
+var Cartas;
+(function (Cartas) {
+    Cartas[Cartas["Magic"] = 0] = "Magic";
+    Cartas[Cartas["Attack"] = 1] = "Attack";
+    Cartas[Cartas["Defense"] = 2] = "Defense";
+    Cartas[Cartas["Trap"] = 3] = "Trap";
+    Cartas[Cartas["Flash"] = 4] = "Flash";
+})(Cartas || (Cartas = {}));
+var cartas = Cartas.Magic;
+//
+var Cores;
+(function (Cores) {
+    Cores[Cores["Branco"] = 0] = "Branco";
+    Cores[Cores["Rosa"] = 1] = "Rosa";
+    Cores[Cores["Preto"] = 2] = "Preto";
+    Cores[Cores["Azul"] = 3] = "Azul";
+    Cores[Cores["Vermelho"] = 4] = "Vermelho";
+    Cores[Cores["Roxo"] = 5] = "Roxo";
+    Cores[Cores["Laranja"] = 6] = "Laranja";
+    Cores[Cores["Cinza"] = 7] = "Cinza";
+    Cores[Cores["Mangento"] = 8] = "Mangento";
+})(Cores || (Cores = {}));
+var AnimalHab;
+(function (AnimalHab) {
+    AnimalHab[AnimalHab["PodeVoar"] = 0] = "PodeVoar";
+})(AnimalHab || (AnimalHab = {}));
+var Triste;
+(function (Triste) {
+    Triste[Triste["False"] = 0] = "False";
+    Triste[Triste["True"] = 1] = "True";
+    Triste[Triste["Unknown"] = 2] = "Unknown";
+})(Triste || (Triste = {}));
+var Feliz;
+(function (Feliz) {
+    Feliz[Feliz["False"] = 0] = "False";
+    Feliz[Feliz["True"] = 1] = "True";
+    Feliz[Feliz["Unknown"] = 2] = "Unknown";
+})(Feliz || (Feliz = {}));
+var Quarta;
+(function (Quarta) {
+    Quarta[Quarta["True"] = 0] = "True";
+    Quarta[Quarta["False"] = 1] = "False";
+    Quarta[Quarta["Unknown"] = 2] = "Unknown";
+})(Quarta || (Quarta = {}));
+var Quinta;
+(function (Quinta) {
+    Quinta[Quinta["False"] = 0] = "False";
+    Quinta[Quinta["True"] = 1] = "True";
+    Quinta[Quinta["Unknown"] = 2] = "Unknown";
+})(Quinta || (Quinta = {}));
+var Terça;
+(function (Terça) {
+    Terça[Terça["False"] = 0] = "False";
+    Terça[Terça["True"] = 1] = "True";
+    Terça[Terça["Unknown"] = 2] = "Unknown";
+})(Terça || (Terça = {}));
+var Tuesday;
+(function (Tuesday) {
+    Tuesday[Tuesday["False"] = 0] = "False";
+    Tuesday[Tuesday["True"] = 1] = "True";
+    Tuesday[Tuesday["Unknown"] = 2] = "Unknown";
+})(Tuesday || (Tuesday = {}));
+var Saturday;
+(function (Saturday) {
+    Saturday[Saturday["False"] = 0] = "False";
+    Saturday[Saturday["True"] = 1] = "True";
+    Saturday[Saturday["Unknown"] = 2] = "Unknown";
+})(Saturday || (Saturday = {}));
+var Thursday;
+(function (Thursday) {
+    Thursday[Thursday["False"] = 0] = "False";
+    Thursday[Thursday["True"] = 1] = "True";
+    Thursday[Thursday["Unknown"] = 2] = "Unknown";
+})(Thursday || (Thursday = {}));
+console.log(Tuesday.True);
+console.log(Terça.True);
+console.log(Quarta.False);
+console.log(Feliz[0]);
+console.log(Feliz.True);
+console.log(Triste[0]);
+var x = 30, y = 40;
+var venus = 60000, earth = 6314;
+var work = "se eu trabalhar ganho dinheiro!!!", nao = "nao trabalhar";
+var luz = 300000, som = 3000;
+if (luz > som) {
+    console.log('luz é mais');
+}
+else if (som > luz) {
+    console.log("Faltou água");
+}
+else if (som === luz) {
+    console.log('isso');
+}
+for (var i = 0; i < 3; i++) {
+    console.log("Bloquear ");
+}
+if (work) {
+    console.log("work)");
+}
+else if (nao) {
+    console.log("nao");
+}
+else if (sim) {
+    console.log("sim ");
+}
+var str = "Hello World";
+for (var char, ofset;;)
+    if (x < y) {
+        console.log("vc é fraco!");
+    }
+if (venus < earth) {
+    console.log("terra é maior que venus");
+}
+else if (earth === venus) {
+    console.log("TERRA PLANNA!!!");
+}
+else if (earth + 1 > venus) {
+    console.log("Terra não é plana");
 }
 function BemVindoo(y) {
     if (Array.isArray(y)) {
--- array.ts
+++ array.ts
@@ -1,7 +1,24 @@
+function aplicarDisconto(preco, disconto = 0.05) {
+      return preco * (2 - disconto);
+}
 
+
+function tirarDicsconto(precu, disconto = 100) {
+      return precu = 100; 
+}
+
+console.log(aplicarDisconto(100));
 
 function exemplo() {
 
+}
+
+function Welcome(x: string[] | string) {
+      if (Array.isArray(x)) {
+            console.log("Ola, " + x.join(" and ")); 
+      } else {
+            console.log("Welcome " + x); 
+      }      
 }
 
 function BemVindo(x: string[] | string) {
@@ -10,6 +27,194 @@ function BemVindo(x: string[] | string) {
    }  else {
           console.log("Bem vindo " + x);
    }
+}
+interface timeReal {
+      a: string;
+      b: string;
+      c: string; 
+      d: string;
+      f: string; 
+      h: string; 
+      j: string;
+      tec: string;
+}
+
+declare var meuTimeReal: timeReal;
+
+interface novoUser {
+      acesso: false;
+      mudarfotodogrupo: false;
+      mandamenssagemacada3s: true;
+       
+}
+
+
+interface Ponto {
+      x: number; y: number;
+}
+
+declare var meuPonto: Ponto;
+
+interface Ponto {
+      z: number;
+}
+
+
+interface Adm {
+      acesso: boolean;
+      apagar: boolean;
+      parar: boolean; 
+      compartilhar: boolean; 
+}
+
+
+interface usrT {
+      accesso: boolean;
+      apagar: boolean;
+      parar: boolean;
+      compartilhar: boolean;
+      mandamenssagemacada3s: false
+}
+
+interface admOne {
+      acesso: string;
+      apagar: boolean; 
+      parar: boolean;
+      compartilhar: boolean;
+      mudarfotodogrupo: string;
+      mandamenssagemacada3s: false;
+}
+
+enum Cartas {
+      Magic = 0,  
+      Attack = 1,
+      Defense = 2, 
+      Trap = 3, 
+      Flash = 4
+}
+
+var cartas = Cartas.Magic; 
+//
+
+
+enum Cores {
+      Branco,
+      Rosa,
+      Preto,
+      Azul, 
+      Vermelho, 
+      Roxo,
+      Laranja, 
+      Cinza,
+      Mangento
+}
+
+enum AnimalHab {
+      PodeVoar  = 0
+}
+
+enum Triste {
+      False,
+      True,
+      Unknown
+}
+
+enum Feliz {
+      False, 
+      True, 
+      Unknown
+}
+
+enum Quarta {   
+      True,
+      False,
+      Unknown
+}
+
+enum Quinta {
+      False, 
+      True, 
+      Unknown 
+}
+
+enum Terça {
+      False, 
+      True, 
+      Unknown
+}
+
+enum Tuesday {
+      False,
+      True, 
+      Unknown
+}
+
+enum Saturday {
+      False,
+      True,
+      Unknown
+}
+
+enum Thursday { 
+      False,
+      True,
+      Unknown
+}
+
+console.log(Tuesday.True); 
+console.log(Terça.True); 
+console.log(Quarta.False);
+console.log(Feliz[0]); 
+console.log(Feliz.True);
+console.log(Triste[0]);
+
+
+let x: number = 30, y = 40;
+let venus: number = 60000, earth = 6314;
+
+let work: string = "se eu trabalhar ganho dinheiro!!!", nao = "nao trabalhar";
+let luz: number = 300000, som = 3000; 
+
+if ( luz > som ) {
+      console.log('luz é mais');
+
+}
+else if ( som > luz ) {
+      console.log("Faltou água");
+}
+else if (som === luz) {
+      console.log('isso')
+}
+
+for (let i = 0; i < 3; i++) {
+      console.log("Bloquear ") 
+}
+if (work) {
+      console.log("work)"; 
+}
+else if (nao) {
+      console.log("nao");
+}
+else if (sim) {
+      console.log("sim ");    
+}
+
+let str = "Hello World"
+
+for (var. char.ofset 
+            )
+if (x < y) {
+      console.log("vc é fraco!");
+}
+
+if (venus < earth) {
+      console.log("terra é maior que venus"); 
+}
+else if ( earth === venus) { 
+      console.log("TERRA PLANNA!!!");
+}
+else if ( earth + 1 > venus) { 
+      console.log("Terra não é plana");
 }
 
 
